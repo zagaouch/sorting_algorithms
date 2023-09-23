012facb68bc5e7465dd14750fe53ae4bd63f95ed
zagaouch <i.zagaouch@gmail.com>
2023-09-23 13:28:43 +0100

    task0

--- print_list.c
+++ print_list.c
@@ -7,16 +7,15 @@
  */
 void print_list(const listint_t *list)
 {
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+int i;
+i = 0;
+while (list)
+{
+if (i > 0)
+printf(", ");
+printf("%d", list->n);
+++i;
+list = list->next;
+}
+printf("\n");
 }
