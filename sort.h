217026d3272e1aec1cf62408b8f80bc09bf0f245
zagaouch <i.zagaouch@gmail.com>
2023-09-23 13:42:16 +0100

    task0

--- sort.h
+++ sort.h
@@ -19,6 +19,7 @@ struct listint_s *next;
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+int main(void);
 
 
 #endif
