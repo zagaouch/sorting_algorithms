924c49504f2bd8b98c88b030eeea3ff27750fc14
zagaouch <i.zagaouch@gmail.com>
2023-09-25 22:52:58 +0100

    task100

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ void insertion_sort_list(listint_t **list);
 listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 
 #endif
