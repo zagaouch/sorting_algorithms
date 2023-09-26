36511605b4f313c55775edcc86e5aeead9c735d3
zagaouch <i.zagaouch@gmail.com>
2023-09-26 01:33:47 +0100

    task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,6 +6,7 @@
  * @array: The array to be sorted
  * @size: The size of the array
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
--- sort.h
+++ sort.h
@@ -1,8 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,20 +14,62 @@
  */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
+/** provided functions */
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/** 0 */
+void bubble_sort(int *array, size_t size);
+
+/** 1 */
 void insertion_sort_list(listint_t **list);
-listint_t *create_listint(const int *array, size_t size);
+void swapem(listint_t *l, listint_t *r, listint_t **h);
+
+/** 2 */
 void selection_sort(int *array, size_t size);
+
+/** 3 */
 void quick_sort(int *array, size_t size);
+int partition(int *a, int l, int h);
+void qs(int *a, int l, int h);
+
+/** advanced */
+
+/** 4 */
 void shell_sort(int *array, size_t size);
+
+/** 5 */
 void cocktail_sort_list(listint_t **list);
 
+/** 6 */
+void counting_sort(int *array, size_t size);
+int findmax(int *array, size_t size);
+int count(int *array, size_t size, int val);
 
+/** 7 */
+void merge_sort(int *array, size_t size);
+void merge(int *array, int low, int middle, int high, int *temp);
+void merge_sorty(int *array, int low, int high, int *temp);
+
+/** 8 */
+void heap_sort(int *array, size_t size);
+
+/** 9 */
+void radix_sort(int *array, size_t size);
+void counting_sort_r(int *array, size_t size, int pos, int *out, int *ca);
+
+/** 10 */
+void bitonic_sort(int *array, size_t size);
+void swapint(int *l, int *r);
+void b_sort(int *array, int low, int count, int dir, size_t size);
+void b_merge(int *array, int low, int count, int dir, size_t size);
+
+/** 11 */
+void quick_sort_hoare(int *array, size_t size);
+int partition_hoare(int *a, int l, int h);
 #endif
