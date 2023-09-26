e0a0ba771fd0d312dd2e03ea10c5f9bc4282a9c3
zagaouch <i.zagaouch@gmail.com>
2023-09-26 01:44:02 +0100

    task2

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,27 +9,39 @@
  *
  * Return: The index of the pivot element after partitioning
  */
-int lomuto_partition(int *array, int low, int high, size_t size)
-{
-	int pivot = array[high];
-	int i = low - 1, j, temp;
 
-	for (j = low; j <= high - 1; j++)
+int lomuto_partition(int *a, int l, int h)
+{
+	int p, i, j, t;
+	static int size, k;
+
+	if (k == 0)
+		size = h + 1, k++;
+	p = a[h];
+	i = l;
+	for (j = l; j < h; j++)
 	{
-		if (array[j] <= pivot)
+		if (a[j] <= p)
 		{
+			if (i != j)
+			{
+				t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+				print_array(a, size);
+			}
 			i++;
-			temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			print_array(array, size);
 		}
 	}
-	temp = array[i + 1];
-	array[i + 1] = array[high];
-	array[high] = temp;
-	print_array(array, size);
-	return (i + 1);
+	if (i != h)
+	{
+		t = a[i];
+		a[i] = a[h];
+		a[h] = t;
+		print_array(a, size);
+	}
+
+	return (i);
 }
 
 
@@ -40,16 +52,19 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  * @high: The ending index of the partition
  * @size: The size of the array
  */
-void quick_sort_recursive(int *array, int low, int high, size_t size)
+void quick_sort_recursive(int a, int l, int h)
 {
-	if (low < high)
-	{
-		int pivot_index = lomuto_partition(array, low, high, size);
+	int p;
 
-		quick_sort_recursive(array, low, pivot_index - 1, size);
-		quick_sort_recursive(array, pivot_index + 1, high, size);
+	if (l < h)
+	{
+		p = partition(a, l, h);
+		qs(a, l, p - 1);
+		qs(a, p + 1, h);
 	}
 }
+
+
 /**
  * quick_sort - Sorts an array of integers in ascending order using Quick sort
  * @array: The array to be sorted
@@ -58,7 +73,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-	quick_sort_recursive(array, 0, size - 1, size);
+	qs(array, 0, size - 1);
 }
